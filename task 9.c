d30b2adf28d321ca2d37b476d8777ff79d80eeab
Nusrat Ahmmed Ekra <141204200+Nusratekra@users.noreply.github.com>
2023-09-20 09:06:22 +0600

    Add files via upload

---
+++ task 9.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char alp[52];
+    int i,vowelCount=0;
+    fgets(alp,sizeof(alp),stdin);
+
+    for(i=0;i<strlen(alp);i++){
+
+        char ch= alp[i];
+
+    if(ch=='a'||ch == 'e'||ch == 'i'||ch =='o'||ch == 'u'||ch=='A'||ch == 'E'||ch == 'I'||ch =='O'||ch == 'U'){
+       vowelCount++;
+    }
+    }
+    printf("%d",vowelCount);
+    return 0;
+}
+
